792ad885c62d6f1ba72dc69c425c37f84487a49b
Б.Сайнбаяр <sainbayar@erdenetmc.mn>
2025-08-27 15:22:03 +0800

    frontoosNomooNemii

--- src/App.js
+++ src/App.js
@@ -4,20 +4,75 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [price, setPrice] = useState("");
+
+  // Номын жагсаалтыг авах функц
+  const fetchBooks = async () => {
+    try{
+      const response = await axios.get("http://localhost:5005/api/books");
+      setBooks(response.data);
+    } catch (error) {
+      console.error("Номын жагсаалтыг авахад алдаа гарлаа:", error);
+    }
+  };
 
   useEffect(() => {
-    axios.get("http://localhost:5005/api/books")
-    .then((response) => {
-      setBooks(response.data);
-    })
-    .catch((error) => {
-      console.error("Error fetching books:", error);
-    });
+    fetchBooks();
   }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+      const newBook = { title, author, genre, price };
+      axios.post("http://localhost:5005/api/books", newBook)
+      .then((response) => {
+        fetchBooks();
+        setTitle("");
+        setAuthor("");
+        setGenre("");
+        setPrice("");
+      })
+      .catch((error) => {
+        console.error("Ном нэмэхэд алдаа гарлаа:", error);
+      });
+  };
 
   return (
     <div className="App">
-      <h1>Номын дэлгүүр</h1>
+      <h1>Ном нэмэх</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Номын нэр"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Зохиолч"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Төрөл"
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          required
+        />
+        <input
+          type="number"
+          placeholder="Үнэ"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
+        <button type="submit">Ном нэмэх</button>
+      </form>
+      <h2>Бүх номын жагсаалт</h2>
       <ul>
         {books.map((book) => (
           <li key={book._id}>
